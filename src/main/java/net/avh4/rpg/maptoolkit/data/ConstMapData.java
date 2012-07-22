2affc67b780aa87bd590981894b55a2412624223
Aaron VonderHaar <gruen0aermel@gmail.com>
2012-07-22 12:40:45 -0700

    Extract ConstMapData interface.

---
+++ src/main/java/net/avh4/rpg/maptoolkit/data/ConstMapData.java
@@ -0,0 +1,15 @@
+package net.avh4.rpg.maptoolkit.data;
+
+import net.avh4.util.Point;
+
+public interface ConstMapData<T> {
+    T getData(Point p);
+
+    T getData(int x, int y);
+
+    T getData(int x, int y, T valueIfNull);
+
+    int getWidth();
+
+    int getHeight();
+}
--- src/main/java/net/avh4/rpg/maptoolkit/data/MapData.java
+++ src/main/java/net/avh4/rpg/maptoolkit/data/MapData.java
@@ -2,20 +2,11 @@ package net.avh4.rpg.maptoolkit.data;
 
 import net.avh4.util.Point;
 
-public interface MapData<T> {
-    T getData(Point p);
-
-    T getData(int x, int y);
-
-    T getData(int x, int y, T valueIfNull);
+public interface MapData<T> extends ConstMapData<T> {
 
     void setData(Point p, T value);
 
     void setData(int x, int y, T value);
 
     void setData(Point p, int dx, int dy, T value);
-
-    int getWidth();
-
-    int getHeight();
 }
