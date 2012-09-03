43bd82e15b195dc9b456fe91437150e1d778a9d9
Aaron VonderHaar <gruen0aermel@gmail.com>
2012-09-03 00:52:20 -0700

    Add DenseMapData constructor with a default value.

--- src/main/java/net/avh4/rpg/maptoolkit/data/DenseMapData.java
+++ src/main/java/net/avh4/rpg/maptoolkit/data/DenseMapData.java
@@ -8,6 +8,15 @@ public class DenseMapData<T> extends MapDataBase<T> {
         data = new Object[maxX][maxY];
     }
 
+    public DenseMapData(String name, int maxX, int maxY, T defaultValue) {
+        this(name, maxX, maxY);
+        for (int y = 0; y < maxY; y++) {
+            for (int x = 0; x < maxX; x++) {
+                data[x][y] = defaultValue;
+            }
+        }
+    }
+
     @SuppressWarnings("unchecked")
     @Override
     public T getData(int x, int y) {
